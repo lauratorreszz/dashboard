b6e0e08e172014b16c2a6f3bc3d8412909b661e7
lauratorreszz <fri0199289@aluno.firjansenaisesi.com.br>
2023-11-28 18:34:00 -0300

    aa

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
-import { GoPrimitiveDot } from 'react-icons/go';
+import { GoAlert } from 'react-icons/go';
 import { IoIosMore } from 'react-icons/io';
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
